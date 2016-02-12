51fa9c5f55d3566d4b8632906cd58d73a295f761
shumingli708 <shumingli708@users.noreply.github.com>
2016-02-12 11:09:34 -0800

    Create shumingli.R
    Replicate R scripts

---
+++ Li/shumingli.R
@@ -0,0 +1,37 @@
+main <- function () {
+  if (!exists("LAControllerDatabase")) {
+    LAControllerDatabase <- read.csv("https://controllerdata.lacity.org/api/views/3ctd-sjrm/rows.csv?accessType=DOWNLOAD")
+  } 
+  mainPhyloPlot(LAControllerDatabase)
+  return(c(	getNumPayments(LAControllerDatabase),
+            getMeanPayment(LAControllerDatabase),
+            getMedianPayment(LAControllerDatabase),
+            getSDPayment(LAControllerDatabase)))
+}
+mainPhyloPlot <- function(LAControllerDatabase) {
+  LAControllerDatabase$EXPENDITURES <- as.numeric(gsub("\\$", "", as.character(LAControllerDatabase$EXPENDITURES)))
+  departmentExpenditure <- aggregate(LAControllerDatabase$EXPENDITURE, by=list(Category=LAControllerDatabase$DEPARTMENT.NAME), FUN=sum)
+  cluster <- hclust((dist(departmentExpenditure[2]))^(1/2), "ave")
+  labels <- t(departmentExpenditure[1])
+  labels <- substring(labels, 0, 10)
+  plot(cluster,  labels, hang = -1, main = "Departments by Net Expenditure")
+}
+
+getNumPayments <- function(LAControllerDatabase) {
+  return (nrow(LAControllerDatabase))
+}
+
+getMeanPayment <- function(LAControllerDatabase) {
+  Expenditures <- as.numeric(gsub("\\$", "", as.character(LAControllerDatabase$EXPENDITURES)))
+  return (mean(Expenditures))
+}
+
+getMedianPayment <- function(LAControllerDatabase) {
+  Expenditures <- as.numeric(gsub("\\$", "", as.character(LAControllerDatabase$EXPENDITURES)))
+  return (median(Expenditures))
+}
+
+getSDPayment <- function(LAControllerDatabase) {
+  Expenditures <- as.numeric(gsub("\\$", "", as.character(LAControllerDatabase$EXPENDITURES)))
+  return (sd(Expenditures))
+}
