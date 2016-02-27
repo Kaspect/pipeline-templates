3005082a501a4be46d39bae38b858ee4a2faf6df
Max Schwarzer <MaxASchwarzer@gmail.com>
2016-02-27 14:35:40 -0800

    Bugfixing python script.  See #11

--- Docker/compiler.py
+++ Docker/compiler.py
@@ -6,6 +6,6 @@ f = open("log.txt", "w")
 if (len(sys.argv) == 1):
 	print "usage: python compiler.py filename"
 filename = str(sys.argv[1])
-r_output = subprocess.check_output(["r Docker/knitscript.r " + filename + ".Rtex"])
-latex_output = subprocess.check_output(["pdflatex *.tex"])
+r_output = subprocess.check_output(["r", "Docker/knitscript.r", filename + ".Rtex"])
+latex_output = subprocess.check_output(["pdflatex", "*.tex"])
 f.write(r_Output + latex_output)
