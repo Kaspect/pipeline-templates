30f65a446695283be2344d700d74928d51cc081a
Max Schwarzer <MaxASchwarzer@gmail.com>
2016-03-10 17:43:30 -0800

    Fixed script.  See #15

--- Docker/api.py
+++ Docker/api.py
@@ -5,11 +5,6 @@ app = Flask(__name__)
 api = Api(app)
 
 class compiler(Resource):
-
-	def get(self) 
-		return{'hello':'world'}
-	
-
 	def put(self):
  	# Get Repo name and git clone
  		repo = request.form['data']
@@ -18,13 +13,13 @@ class compiler(Resource):
  		pipe.wait()
  
  	# Get file location and call complie to create PDF and log
- 		urlArr = repo.split(/)
+ 		urlArr = repo.split("/")
  		relevantBit = urlArr[-1]
  		compiler.py(relevantBit+"\Root.Rtex")
  
  		# Put files in a zip
  		memory_file = BytesID()
- 		with zipfile.ZipFile(memory_file, 'w' as zf:
+ 		with zipfile.ZipFile(memory_file, 'w') as zf:
  			zf.write('Root.pdf')
  			zf.write('log.txt')
  		memory_file.seek(0)
@@ -34,7 +29,7 @@ class compiler(Resource):
  		strRet = urlArr(-1) + ".zip"
  		return send_file(memory_file, attachment_filename=strRet, as_attachment=True) 
 
-api.add_resource(complier)
+api.add_resource(compiler)
 
 
 if __name__ == '__main__':
