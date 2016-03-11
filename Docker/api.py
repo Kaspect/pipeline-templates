a7911101ad5e59b23134583f725960e574af9e20
Shuming Li <shumingli@berkeley.edu>
2016-03-10 16:50:47 -0800

    add put function to api.py see#15

--- Docker/api.py
+++ Docker/api.py
@@ -4,11 +4,33 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class complier(Resource):
 
-api.add_resource(HelloWorld, '/')
+	def put(self):
+ 	# Get Repo name and git clone
+ 		repo = request.form['data']
+ 		cmd = 'git clone ' + repo
+  		pipe = subprocess.Popen(cmd, shell=True)
+ 		pipe.wait()
+ 
+ 	# Get file location and call complie to create PDF and log
+ 		urlArr = repo.split(/)
+ 		relevantBit = urlArr[-1]
+ 		compiler.py(relevantBit+"\Root.Rtex")
+ 
+ 		# Put files in a zip
+ 		memory_file = BytesID()
+ 		with zipfile.ZipFile(memory_file, 'w' as zf:
+ 			zf.write('Root.pdf')
+ 			zf.write('log.txt')
+ 		memory_file.seek(0)
+ 
+ 
+ 		# Return Zip
+ 		strRet = urlArr(-1) + ".zip"
+ 		return send_file(memory_file, attachment_filename=strRet, as_attachment=True) 
+
+api.add_resource(complier)
 
 if __name__ == '__main__':
     app.run(debug=True)
