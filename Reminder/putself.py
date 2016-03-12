cdbf0468dd16c8e500d9b36c031b3635659988f0
Nick Reminder <Nick Reminder>
2016-03-11 18:41:25 -0800

    Updated PUT function for error catching. See #15

--- Reminder/putself.py
+++ Reminder/putself.py
@@ -1,23 +1,30 @@
 def put(self):
-	# Get Repo name and git clone
-	repo = request.form['data']
-	cmd = 'git clone ' + repo
-	pipe = subprocess.Popen(cmd, shell=True)
-	pipe.wait()
+	try:
 
-	# Get file location and call complie to create PDF and log
-	urlArr = repo.split(/)
-	relevantBit = urlArr[-1]
-	compiler.py(relevantBit+"\Root.Rtex")) 
+		# Get Repo name and git clone
+		repo = request.form['data']
+		cmd = 'git clone ' + repo
+		pipe = subprocess.Popen(cmd, shell=True)
+		pipe.wait()
 
-	# Put files in a zip
-	memory_file = BytesID()
-	with zipfile.ZipFile(memory_file, 'w' as zf:
-		zf.write('Root.pdf')
-		zf.write('log.txt')
-	memory_file.seek(0)
+		# Get file location and call complie to create PDF and log
+		urlArr = repo.split(/)
+		relevantBit = urlArr[-1]
+		compiler.py(relevantBit+"\Root.Rtex")) 
+
+		# Put files in a zip
+		memory_file = BytesID()
+		with zipfile.ZipFile(memory_file, 'w' as zf:
+			zf.write('Root.pdf')
+			zf.write('log.txt')
+		memory_file.seek(0)
 
 
-	# Return Zip
-	strRet = urlArr(-1) + ".zip"
-	return send_file(memory_file, attachment_filename=strRet, as_attachment=True) 
+		# Return Zip
+		strRet = urlArr(-1) + ".zip"
+		return send_file(memory_file, attachment_filename=strRet, as_attachment=True) 
+
+	except:
+
+		e = sys.exc_info()[0]
+		write_to_page( "<p>Error: %s</p>" % e )
