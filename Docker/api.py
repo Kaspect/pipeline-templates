6ccdd56b3fa55061b99017e4cffa5ca5028f0f03
Nick Reminder <Nick Reminder>
2016-03-22 22:22:19 -0700

    Updated with import zipfile. See #15

--- Docker/api.py
+++ Docker/api.py
@@ -2,12 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api			
 import sys
 import subprocess
-<<<<<<< HEAD
 import zipfile
-=======
-import logging
-import io
->>>>>>> fa27f2ea943efb86959eaffad3a81bd37b9c93f7
 
 
 app = Flask(__name__)
@@ -21,24 +16,24 @@ class compiler(Resource):
 		return "Hello!"
 
 	def put(self):
-		logging.warning("Started put command")
+		print("Started put command")
 		try:
 			# Get Repo name and git clone
 			repo = request.form['data']
-			logging.warning("Got repo address from data")
+			print("Got repo address from data")
 			cmd = 'git clone ' + repo
 			pipe = subprocess.Popen(cmd, shell=True)
 			
-			logging.warning("Started clone command")
+			print("Started clone command")
 			pipe.wait()
 			
-			logging.warning("Finished clone command")
+			print("Finished clone command")
 
 			# Get file location and call complie to create PDF and log
 			urlArr = repo.split("/")
 			relevantBit = urlArr[-1]
 			filename = relevantBit+"/Root.Rtex"
-			logging.warning("Got address of Rtex in repo")
+			print("Got address of Rtex in repo")
 
 
 			#create output file
@@ -47,15 +42,15 @@ class compiler(Resource):
 			#Compile the file in knitr and save the output, including if there's an error.
 			try:
 				r_output = subprocess.check_output(["r", "pipeline-templates/Docker/knitscript.r", filename])
-			except Exception as e:
-				r_output = str(e)
+			except Exception, e:
+				r_output = str(e.output)
 
 			#Compile the latex, but save the output if there's an error
 			try:
 				latex_output = subprocess.check_output(["pdflatex -interaction nonstopmode *.tex"], shell=True, stderr=subprocess.STDOUT)
 				
-			except Exception as e:
-				latex_output = str(e)
+			except Exception, e:
+				latex_output = str(e.output)
 				#Combine the outputs and write them to log.txt
 				
 			final_output = r_output + latex_output
@@ -63,11 +58,11 @@ class compiler(Resource):
 
 			#compiler.py(relevantBit+"\Root.Rtex") 
 			# Put files in a zip
-			memory_file = io.BytesIO()
+			memory_file = BytesID()
 			with zipfile.ZipFile(memory_file, 'w') as zf:
 				zf.write('Root.pdf')
 				zf.write('log.txt')
-				logging.warning("Wrote to zip")
+				print("Wrote ")
 			memory_file.seek(0)
 
 
@@ -75,9 +70,9 @@ class compiler(Resource):
 			strRet = urlArr[-1] + ".zip"
 			return send_file(memory_file, attachment_filename=strRet, as_attachment=True) 
 
-		except Exception as e:
-			logging.warning(str(e))
-			return (str(e))
+		except Exception, e:
+			e = sys.exc_info()[0]
+			print(write_to_page( "<p>Error: %s</p>" % e ))
 			
 api.add_resource(compiler,"/")
 
